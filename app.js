5e72fb573a559f267d3f717fa7682d32f5fa9b6f
amanpallav <amanpallav@gmail.com>
2021-01-09 00:54:38 +0530

    output box done

--- app.js
+++ app.js
@@ -11,10 +11,32 @@ var outputDiv = document.querySelector("#output");
 //         console.log("Input",txtInput.value);
 // } )
 
-btnTranslate.addEventListener("click",clickHandler)
+// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
+function getTranslationURL(text)
+{
+        return serverURL + "?" + "text=" + text
+}
+
+function errorHandler(error)
+{
+        console.log("error occured",error);
+        alert("something went wrong with this server, try again later");
+}
 
 function clickHandler(){
-        outputDiv.innerText = "gugydfugiguj"+txtInput.value;
-}
+        var inputText = txtInput.Value;
+        fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+                var translatedText = json.contents.translated;
+                outputDiv.innerText = translatedText;
+                })
+        .catch(errorHandler)
+};
+
+btnTranslate.addEventListener("click",clickHandler)
+
 
 
